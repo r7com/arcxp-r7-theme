58f3c548b75ec2be03ab2e9c2810afe876a69306
rsmaciel <rsmaciel@recordtv.com.br>
2023-09-11 22:34:06 -0300

    chore: intial configuration lints and code style

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  plugins: ["jsx-a11y", "prettier"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2015
+  },
+  "env": {
+    "node": true
+  },
+  overrides: [
+    {
+      files: ["*.test.js", "__mocks__/**"],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
+      extends: [
+        "plugin:jsx-a11y/recommended",
+        "plugin:prettier/recommended"
+      ],
+      rules: {
+        semi: 0,
+        "prettier/prettier": ["error", { "semi": false }]
+      }
+    },
+  ],
+}
--- components/chains/generators/list.jsx
+++ components/chains/generators/list.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import React from 'react'
 
-export default (ListType, ItemType = 'li') => ({ children }) =>
-  <ListType>
-    {
-      children.map(
-        (child, index) =>
+export default (ListType, ItemType = 'li') =>
+  ({ children }) =>
+    (
+      <ListType>
+        {children.map((child, index) => (
           <ItemType key={child.key || index}>{child}</ItemType>
-      )
-    }
-  </ListType>
+        ))}
+      </ListType>
+    )
