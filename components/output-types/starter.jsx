31677bb6375fc0df3061930eacfdda2b899f44c1
Kiryll Semenov <king12@sofware-automation.store>
2023-09-21 19:17:25 +0300

    feat: created starter output type. Added condition to show noindex meta-tag

---
+++ components/output-types/starter.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useFusionContext } from 'fusion:context'
+import getProperties from 'fusion:properties'
+
+function StarterOutputType({
+  children,
+  contextPath,
+  deployment,
+  CssLinks,
+  Fusion,
+  Libs,
+  MetaTags,
+}) {
+  const { globalContent, arcSite } = useFusionContext()
+  const { locale = 'en' } = getProperties(arcSite)
+  return (
+    <html lang={locale}>
+      <head>
+        <title>Fusion Article</title>
+        {globalContent?.label?.noindex?.text === 'Yes' && <meta name="robots" content="noindex" />}
+        <MetaTags />
+        <Libs />
+        <CssLinks />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href={deployment(`${contextPath}/resources/favicon.ico`)}
+        />
+      </head>
+      <body>
+        <div id="fusion-app">{children}</div>
+        <Fusion />
+      </body>
+    </html>
+  )
+}
+
+export default StarterOutputType
