b10f00f7b5e508e4ad0e4ab428cccc635140414f
Kiryll Semenov <king12@sofware-automation.store>
2023-10-04 11:29:44 +0300

    fix: added condition to show section title as a link. Updated npmrc

--- components/features/sitemap-footer/Section/index.jsx
+++ components/features/sitemap-footer/Section/index.jsx
@@ -6,7 +6,19 @@ import { SectionItem } from '../SectionItem'
 export const Section = ({ section }) => {
   return (
     <SitemapFooter.MenuItem>
-      <SitemapFooter.MenuTitle>{section.name}</SitemapFooter.MenuTitle>
+      <SitemapFooter.MenuTitle>
+        {section?.navigation?.menu_url ? (
+          <SitemapFooter.MenuLik
+            openInNewTab={true}
+            title={section.navigation.nav_title}
+            url={section.navigation.menu_url}
+          >
+            {section.navigation.nav_title}
+          </SitemapFooter.MenuLik>
+        ) : (
+          section?.navigation?.nav_title
+        )}
+      </SitemapFooter.MenuTitle>
       <SitemapFooter.Submenu>
         {section?.children.length
           ? section?.children.map(sectionItem => (
