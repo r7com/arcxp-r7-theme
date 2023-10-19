d5f66fba302f9bf81e2adc79c2f742ef237f84a4
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-10-19 09:58:36 -0300

    refactor(newsblock): renaming card to news

--- components/features/news-card/default.jsx
+++ components/features/news-card/default.jsx
@@ -50,9 +50,29 @@ NewsCard.icon = 'paragraph-image-left'
 
 NewsCard.propTypes = {
   customFields: PropTypes.shape({
-    cardType: PropTypes.oneOf(['title', 'imageAbove', 'titleOverImage', 'titleToTheSides']),
-    cardTitle: PropTypes.string,
-    imageFormat: PropTypes.oneOf(['square', 'landscape', 'portrait']),
+    cardType: PropTypes.oneOf(['title', 'imageAbove', 'titleOverImage', 'titleToTheSides']).tag({
+      label: 'Formato da notícia',
+      defaultValue: 'title',
+      labels: {
+        title: 'Título',
+        imageAbove: 'Imagem acima',
+        titleOverImage: 'Título sobre a imagem',
+        titleToTheSides: 'Título na lateral',
+      },
+    }),
+    cardTitle: PropTypes.string.tag({
+      label: 'Título da notícia',
+    }),
+    imageFormat: PropTypes.oneOf(['square', 'landscape', 'portrait']).tag({
+      label: 'Formato da imagem',
+      group: 'Configurações da imagem',
+      labels: {
+        square: 'Quadrada',
+        landscape: 'Retangular',
+        portrait: 'Retrato',
+      },
+      defaultValue: 'square',
+    }),
     labelType: PropTypes.oneOf([
       'live',
       'blog',
@@ -61,15 +81,38 @@ NewsCard.propTypes = {
       'voting',
       'podcast',
       'aclr',
-    ]),
-    // por default no hatTYpe
+    ]).tag({
+      label: 'Etiqueta da imagem',
+      group: 'Configurações da imagem',
+      labels: {
+        live: 'Ao vivo',
+        blog: 'Blog',
+        studio: 'R7 Estúdio',
+        'sponsored-by': 'Oferecido por',
+        voting: 'Votação',
+        podcast: 'Podcast',
+        aclr: 'ACRL',
+      },
+    }),
+    // revisar nomenclaturas
     hatType: PropTypes.oneOf([
       'alert-with-image',
       'alert-without-image',
       'with-image',
       'without-image',
-    ]),
-    display: PropTypes.boolean,
+    ]).tag({
+      label: 'Tipo de chapéu',
+      labels: {
+        'alert-with-image': 'Breaking news com imagem',
+        'alert-without-image': 'Breaking news',
+        'with-image': 'Com imagem',
+        'without-image': 'Padrão',
+      },
+      defaultValue: 'without-image',
+    }),
+    display: PropTypes.boolean.tag({
+      label: 'Exibir notícia',
+    }),
   }),
 }
 
