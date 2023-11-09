e3a5e8a088682cf9cdcb2cee38fd3a67782b66ca
Kiryll Semenov <king12@sofware-automation.store>
2023-11-09 19:46:43 +0200

    feat: provided custom layout styling

--- components/layouts/article/default.jsx
+++ components/layouts/article/default.jsx
@@ -1,9 +1,8 @@
+import './default.scss'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Stack } from '@wpmedia/arc-themes-components'
 
-const LAYOUT_CLASS_NAME = 'b-right-rail-advanced'
-
+const LAYOUT_CLASS_NAME = 'b-article-layout'
 const ArticleLayout = ({ children }) => {
   const [
     navigation,
@@ -21,39 +20,33 @@ const ArticleLayout = ({ children }) => {
   return (
     <div className={LAYOUT_CLASS_NAME}>
       {navigation ? (
-        <Stack as="header" className={`${LAYOUT_CLASS_NAME}__navigation`}>
-          {navigation}
-        </Stack>
+        <header className={`${LAYOUT_CLASS_NAME}__navigation`}>{navigation}</header>
       ) : null}
-      {fullwidthWithoutContainer ? <Stack>{fullwidthWithoutContainer}</Stack> : null}
+      {fullwidthWithoutContainer ? (
+        <div className={`${LAYOUT_CLASS_NAME}__full-width-without-container`}>
+          {fullwidthWithoutContainer}
+        </div>
+      ) : null}
       <section role="main" tabIndex="-1" className={`${LAYOUT_CLASS_NAME}__main`}>
         {fullwidth1 ? (
-          <Stack className={`${LAYOUT_CLASS_NAME}__full-width-1`}>{fullwidth1}</Stack>
+          <div className={`${LAYOUT_CLASS_NAME}__full-width first`}>{fullwidth1}</div>
         ) : null}
-        <Grid className={`${LAYOUT_CLASS_NAME}__rail-container`}>
-          <Stack className={`${LAYOUT_CLASS_NAME}__main-interior-item`}>
-            <Stack className={`${LAYOUT_CLASS_NAME}__main-interior-item-1`}>{main}</Stack>
-            <Stack className={`${LAYOUT_CLASS_NAME}__main-interior-item-2`}>{main2}</Stack>
-          </Stack>
-          <Stack className={`${LAYOUT_CLASS_NAME}__main-right-rail`}>
-            <Stack className={`${LAYOUT_CLASS_NAME}__main-right-rail-top`}>{rightRailTop}</Stack>
-            <Stack className={`${LAYOUT_CLASS_NAME}__main-right-rail-middle`}>
-              {rightRailMiddle}
-            </Stack>
-            <Stack className={`${LAYOUT_CLASS_NAME}__main-right-rail-bottom`}>
-              {rightRailBottom}
-            </Stack>
-          </Stack>
-        </Grid>
+        <div className={`${LAYOUT_CLASS_NAME}__rail-container`}>
+          <div className={`${LAYOUT_CLASS_NAME}__main-interior-item`}>
+            <div className={`${LAYOUT_CLASS_NAME}__main-interior-item-top`}>{main}</div>
+            <div className={`${LAYOUT_CLASS_NAME}__main-interior-item-bottom`}>{main2}</div>
+          </div>
+          <div className={`${LAYOUT_CLASS_NAME}__main-right-rail`}>
+            <div className={`${LAYOUT_CLASS_NAME}__main-right-rail-top`}>{rightRailTop}</div>
+            <div className={`${LAYOUT_CLASS_NAME}__main-right-rail-middle`}>{rightRailMiddle}</div>
+            <div className={`${LAYOUT_CLASS_NAME}__main-right-rail-bottom`}>{rightRailBottom}</div>
+          </div>
+        </div>
         {fullWidth2 ? (
-          <Stack className={`${LAYOUT_CLASS_NAME}__full-width-2`}>{fullWidth2}</Stack>
+          <div className={`${LAYOUT_CLASS_NAME}__full-width second`}>{fullWidth2}</div>
         ) : null}
       </section>
-      {footer ? (
-        <Stack as="footer" className={`${LAYOUT_CLASS_NAME}__footer`}>
-          {footer}
-        </Stack>
-      ) : null}
+      {footer ? <footer className={`${LAYOUT_CLASS_NAME}__footer`}>{footer}</footer> : null}
     </div>
   )
 }
