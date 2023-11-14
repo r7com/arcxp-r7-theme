534504f01e0b40fa3a1a12b4aa7cf4e0af334387
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-11-14 11:27:37 -0300

    feat(news-list): added four-images template

---
+++ components/features/ news-list/components/four-images/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { News } from './news'
+import './index.scss'
+
+export const FourImages = ({ content }) => {
+  const maxNews = 4
+  const data = content?.slice(0, maxNews)
+  return (
+    <div className="four-images">
+      {data.map(item => (
+        <News key={item._id} content={item} />
+      ))}
+    </div>
+  )
+}
---
+++ components/features/ news-list/components/four-images/news/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card } from '@r7/ui-card'
+import '@r7/ui-card/style.css'
+
+export const News = ({ content }) => {
+  // note: collections items doesn't have the 'elements' to find the first image, so we need to have lead_art
+  const imageToUse = content?.promo_items?.lead_art || {
+    alt_text: 'Logomarga do Portal R7',
+    url: 'https://img.r7.com/images/r7-30072019142631584?crop_position=c',
+  }
+
+  return (
+    <Card className="flex-col">
+      <Card.Image className="mb-xxxs" format="landscape">
+        <img alt={imageToUse.alt_text} src={imageToUse.url} className="w-full" />
+      </Card.Image>
+      <div>
+        <Card.HatWrapper>
+          <Card.HatTitle>{content?.taxonomy?.primary_section?.name}</Card.HatTitle>
+        </Card.HatWrapper>
+        <Card.Title fontStyle="heading-level-3">{content?.headlines?.basic}</Card.Title>
+      </div>
+    </Card>
+  )
+}
