9669fbcf547e84300a45019ed49742ab213add83
Alescia Fernandes <alfernandes@recordtv.com.br>
2023-11-22 15:16:03 -0300

    feat(news-list): added size parameter in collections request

--- content/sources/collections.js
+++ content/sources/collections.js
@@ -1,12 +1,17 @@
 const resolve = function resolve(key) {
   const website = key['arc-site']
-  const fromQuery = key.from !== '' ? '&from=' + key.from : ''
-  return `/content/v4/collections/?_id=${key._id}&website=${website}${fromQuery}`
+  const fromQuery = key.from !== '' && key.from !== undefined ? '&from=' + key.from : ''
+  const sizeQuery = key.size !== '' && key.size !== undefined ? '&size=' + key.size : ''
+  console.log(
+    'request',
+    `/content/v4/collections/?_id=${key._id}&website=${website}${fromQuery}${sizeQuery}`,
+  )
+  return `/content/v4/collections/?_id=${key._id}&website=${website}${fromQuery}${sizeQuery}`
 }
 
 module.exports = {
   resolve,
   schemaName: 'feeds',
-  params: { _id: 'text', from: 'text' },
+  params: { _id: 'text', from: 'text', size: 'text' },
   searchable: 'collection',
 }
