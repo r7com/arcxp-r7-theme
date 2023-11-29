0a43ebe1afc2c478bb5bdfa07b59ca894690ef84
willian <whira@recordtv.com.br>
2023-11-29 17:03:51 -0300

    feat(sitemap-footer): updating responsive layout

--- components/features/sitemap-footer/default.jsx
+++ components/features/sitemap-footer/default.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { SitemapFooter } from '@r7/ui-footer-delivery'
 import PropTypes from 'prop-types'
 import { useContent } from 'fusion:content'
-import { Column } from './Column'
 
 const SitemapFooterBlock = props => {
   const { config } = props.customFields
@@ -11,12 +10,40 @@ const SitemapFooterBlock = props => {
     source: config?.contentService,
     query: config?.contentConfigValues,
   })
-  const columns = content?.children[0]?.children
+  if (!content) return null
+  const columns = content?.children
   return (
     <SitemapFooter.Root>
-      <SitemapFooter.Container>
-        {columns?.length ? columns.map(column => <Column key={column._id} column={column} />) : ''}
-      </SitemapFooter.Container>
+      <SitemapFooter.MenuList>
+        {columns.length > 0 &&
+          columns.map(column => (
+            <SitemapFooter.MenuItem key={column._id}>
+              <SitemapFooter.MenuTitle>
+                <SitemapFooter.Dropdown id={column._id}>
+                  <SitemapFooter.MenuLink title={column.name} href={column.navigation?.menu_url}>
+                    {column.name}
+                  </SitemapFooter.MenuLink>
+                </SitemapFooter.Dropdown>
+              </SitemapFooter.MenuTitle>
+              <SitemapFooter.Submenu id={column._id}>
+                {column.children.length > 0 &&
+                  column.children.map(
+                    submenu =>
+                      submenu.navigation && (
+                        <SitemapFooter.SubmenuItem key={submenu._id}>
+                          <SitemapFooter.MenuLink
+                            title={submenu.navigation.nav_title}
+                            href={submenu.navigation.menu_url}
+                          >
+                            {submenu.name}
+                          </SitemapFooter.MenuLink>
+                        </SitemapFooter.SubmenuItem>
+                      ),
+                  )}
+              </SitemapFooter.Submenu>
+            </SitemapFooter.MenuItem>
+          ))}
+      </SitemapFooter.MenuList>
     </SitemapFooter.Root>
   )
 }
