f061351be0d9627eceb028c6fbdf81f73b2125c2
Jorge Santos <jldsantos@rederecord.com.br>
2023-12-26 19:18:41 -0300

    feat(two-cards-square-photo): adds custom fields for labels and 'Oferecido por'

--- components/features/two-cards-square-photo/default.jsx
+++ components/features/two-cards-square-photo/default.jsx
@@ -7,7 +7,7 @@ import getProperties from 'fusion:properties'
 import { Image } from '@wpmedia/arc-themes-components'
 import getResizeParamsFromANSImage from '../../../util/get-resize-params-from-ans-image'
 import '@r7/ui-card/style.css'
-import { TwoCardsSquarePhoto } from '@r7/ui-card'
+import { TwoCardsSquarePhoto, Card } from '@r7/ui-card'
 
 export const TwoCardsSquarePhotoBlock = props => {
   const { config, display } = props.customFields
@@ -23,6 +23,23 @@ export const TwoCardsSquarePhotoBlock = props => {
     },
   })
 
+  const customFields = [1, 2].map(field => {
+    return {
+      displayLabel: props.customFields[`displayLabel${field}`],
+      label: props.customFields[`label${field}`],
+      sponsoredBy: props.customFields[`sponsoredBy${field}`],
+      sponsoredByImage: props.customFields[`sponsoredByImage${field}`],
+      sponsoredByTitle: props.customFields[`sponsoredByTitle${field}`],
+      sponsoredByImageDesc: props.customFields[`sponsoredByImageDesc${field}`],
+      sponsoredByUrl: props.customFields[`sponsoredByUrl${field}`],
+    }
+  })
+
+  const blocks = [
+    { ...content?.content_elements[0], ...customFields[0] },
+    { ...content?.content_elements[1], ...customFields[1] },
+  ]
+
   if (!display && isAdmin) {
     return <p>Este bloco está oculto. Mude suas configurações para exibí-lo.</p>
   }
@@ -31,14 +48,20 @@ export const TwoCardsSquarePhotoBlock = props => {
     return <p>É necessário definir uma fonte de conteúdo para este bloco.</p>
   }
 
+  const LABEL_BY_SITE = {
+    '/prisma': 'blog',
+    '/estudio': 'studio',
+  }
+
   return (
     display &&
     content && (
       <TwoCardsSquarePhoto>
-        {content.content_elements.map(item => (
+        {blocks.map(item => (
           <TwoCardsSquarePhoto.Item
             key={item._id}
             hat={item.taxonomy?.primary_section?.name}
+            title={item.headlines?.basic}
             renderImage={
               <>
                 {item.promo_items?.basic ? (
@@ -61,9 +84,28 @@ export const TwoCardsSquarePhotoBlock = props => {
                     style={{ objectFit: 'cover' }}
                   />
                 )}
+
+                {(item.sponsoredBy ||
+                  (item.displayLabel && item.label) ||
+                  (item.displayLabel &&
+                    item.label === 'automatic' &&
+                    LABEL_BY_SITE[item.taxonomy?.primary_site?.path])) && (
+                  <Card.Label
+                    sponsoredByImage={item.sponsoredByImage ?? fallbackImage}
+                    sponsoredByImageDesc={item.sponsoredByImageDesc}
+                    sponsoredByTitle={item.sponsoredByTitle}
+                    sponsoredByUrl={item.sponsoredByUrl}
+                    type={
+                      item.sponsoredBy
+                        ? 'sponsored-by'
+                        : item.label === 'automatic'
+                        ? LABEL_BY_SITE[item.taxonomy?.primary_site?.path]
+                        : item.label
+                    }
+                  />
+                )}
               </>
             }
-            title={item.headlines?.basic}
           />
         ))}
       </TwoCardsSquarePhoto>
@@ -73,17 +115,116 @@ export const TwoCardsSquarePhotoBlock = props => {
 
 TwoCardsSquarePhotoBlock.label = 'Two Cards Square Photo - R7 Block'
 
-TwoCardsSquarePhotoBlock.icon = 'tags'
-
 TwoCardsSquarePhotoBlock.propTypes = {
   customFields: PropTypes.shape({
+    display: PropTypes.boolean.tag({
+      label: 'Exibir bloco',
+      defaultValue: true,
+    }),
+    displayLabel1: PropTypes.boolean.tag({
+      group: '1. Label',
+      label: 'Exibir label',
+      defaultValue: true,
+    }),
+    label1: PropTypes.oneOf([
+      'automatic',
+      'live',
+      'blog',
+      'studio',
+      'voting',
+      'podcast',
+      'aclr',
+    ]).tag({
+      group: '1. Label',
+      label: 'Template',
+      labels: {
+        automatic: 'Automático (padrão)',
+        live: 'Ao vivo',
+        blog: 'Blog',
+        studio: 'Estúdio',
+        voting: 'Votação',
+        podcast: 'Podcast',
+        aclr: 'ACLR',
+      },
+      defaultValue: 'automatic',
+    }),
+    sponsoredBy1: PropTypes.boolean.tag({
+      label: 'Exibir oferecido por',
+      group: '1. Oferecido por',
+    }),
+    sponsoredByImage1: PropTypes.string.tag({
+      group: '1. Oferecido por',
+      label: 'Imagem (24px largura)',
+      searchable: 'image',
+    }),
+    sponsoredByImageDesc1: PropTypes.string.tag({
+      group: '1. Oferecido por',
+      label: 'Descrição da imagem (alt)',
+      description: 'Descreva o que você vê na imagem',
+    }),
+    sponsoredByTitle1: PropTypes.string.tag({
+      group: '1. Oferecido por',
+      label: 'Titulo',
+      description: 'Descreva o conteúdo do link destino',
+    }),
+    sponsoredByUrl1: PropTypes.string.tag({
+      group: '1. Oferecido por',
+      label: 'URL (https)',
+    }),
+    displayLabel2: PropTypes.boolean.tag({
+      group: '2. Label',
+      label: 'Exibir label',
+      defaultValue: true,
+    }),
+    label2: PropTypes.oneOf([
+      'automatic',
+      'live',
+      'blog',
+      'studio',
+      'voting',
+      'podcast',
+      'aclr',
+    ]).tag({
+      group: '2. Label',
+      label: 'Template',
+      labels: {
+        automatic: 'Automático (padrão)',
+        live: 'Ao vivo',
+        blog: 'Blog',
+        studio: 'Estúdio',
+        voting: 'Votação',
+        podcast: 'Podcast',
+        aclr: 'ACLR',
+      },
+      defaultValue: 'automatic',
+    }),
+    sponsoredBy2: PropTypes.boolean.tag({
+      label: 'Exibir oferecido por',
+      group: '2. Oferecido por',
+    }),
+    sponsoredByImage2: PropTypes.string.tag({
+      group: '2. Oferecido por',
+      label: 'Imagem (24px largura)',
+      searchable: 'image',
+    }),
+    sponsoredByImageDesc2: PropTypes.string.tag({
+      group: '2. Oferecido por',
+      label: 'Descrição da imagem (alt)',
+      description: 'Descreva o que você vê na imagem',
+    }),
+    sponsoredByTitle2: PropTypes.string.tag({
+      group: '2. Oferecido por',
+      label: 'Titulo',
+      description: 'Descreva o conteúdo do link destino',
+    }),
+    sponsoredByUrl2: PropTypes.string.tag({
+      group: '2. Oferecido por',
+      label: 'URL (https)',
+    }),
     config: PropTypes.contentConfig().tag({
       group: 'Configurar conteúdo',
       label: 'Fonte de conteúdo',
       searchable: 'image',
-    }),
-    display: PropTypes.boolean.tag({
-      label: 'Exibir bloco',
     }),
   }),
 }
