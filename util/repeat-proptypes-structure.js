66f352a536fcaab52b4c476b950669407e8a4655
rafaelfs96 <rafaelfs96@outlook.com>
2023-12-28 11:03:23 -0300

    docs(utils): change jsdoc documentation structure

--- util/repeat-proptypes-structure.js
+++ util/repeat-proptypes-structure.js
@@ -1,6 +1,36 @@
 /**
  * @param {{count: number; shapeTemplate: Function}} params
  * @returns {Object}
+ * @example
+ * How to use:
+ * ```
+ *  MyComponent.propTypes = {
+ *    customFields: PropTypes.shape({
+ *      ...repeatProptypeStructure({ count: 2, shapeTemplate: counter => {
+ *        return {
+ *          [`prop${counter}`]: PropTypes.string.tag({
+ *            group: `group number ${counter}`,
+ *            label: 'my prop'
+ *          })
+ *        }
+ *      }})
+ *    })
+ *  }
+ * ```
+ *
+ * Output:
+ * ```
+ *  {
+ *    prop1: PropTypes.string.tag({
+ *      group: `group number 1`,
+ *      label: 'my prop'
+ *    }),
+ *    prop2: PropTypes.string.tag({
+ *      group: `group number 2`,
+ *      label: 'my prop'
+ *    })
+ *  }
+ * ```
  */
 export function repeatProptypeStructure({ count, shapeTemplate }) {
   let counter = 1
@@ -15,42 +45,3 @@ export function repeatProptypeStructure({ count, shapeTemplate }) {
 
   return obj
 }
-
-/**
- * Input:
- *  repeatProptypeStructure({ count: 2, shapeTemplate: counter => {
- *    return {
- *      [`prop${counter}`]: PropTypes.string.tag({
- *        group: `group number ${counter}`,
- *        label: 'my prop'
- *       })
- *    }
- *  }})
- *
- * Output:
- *  {
- *    prop1: PropTypes.string.tag({
- *      group: `group number 1`,
- *      label: 'my prop'
- *    }),
- *    prop2: PropTypes.string.tag({
- *      group: `group number 2`,
- *      label: 'my prop'
- *    })
- *  }
- *
- * How to use:
- *  MyComponent.propTypes = {
- *    customFields: PropTypes.shape({
- *      ...repeatProptypeStructure({ count: 2, shapeTemplate: counter => {
- *        return {
- *          [`prop${counter}`]: PropTypes.string.tag({
- *            group: `group number ${counter}`,
- *            label: 'my prop'
- *          })
- *        }
- *      }})
- *    })
- *  }
- *
- */
