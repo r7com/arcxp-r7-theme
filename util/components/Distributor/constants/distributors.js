facea8d442cb98bbf8d0469fe2eda1c76619060e
Kiryll Semenov <king12@sofware-automation.store>
2024-01-03 12:51:17 +0200

    feat: extend distributors list

--- util/components/Distributor/constants/distributors.js
+++ util/components/Distributor/constants/distributors.js
@@ -9,4 +9,29 @@ export const DISTRIBUTORS = [
     agency_image: 'https://img.r7.com/images/folha-financeira-26062023151608554',
     agency_url: 'https://folhafinanceira.com.br',
   },
+  {
+    agency_name: 'Lance',
+    agency_image: 'https://img.r7.com/images/logo-lance-16062023142548616',
+    agency_url: '/lance',
+  },
+  {
+    agency_name: 'PORTAL SÃO PAULINO',
+    agency_image: 'https://img.r7.com/images/portaltricolor-15062022171236002',
+    agency_url: 'https://portaldosaopaulino.com.br',
+  },
+  {
+    agency_name: 'O Gol',
+    agency_image: 'https://img.r7.com/images/ogol-22112022140329357',
+    agency_url: 'https://www.ogol.com.br',
+  },
+  {
+    agency_name: 'Papo na Colina',
+    agency_image: 'https://img.r7.com/images/papo-na-colina-09052022105354386',
+    agency_url: 'https://paponacolina.com.br',
+  },
+  {
+    agency_name: 'NETFLU',
+    agency_image: 'https://img.r7.com/images/netflu-10052022120351610',
+    agency_url: 'https://www.netflu.com.br',
+  },
 ]
