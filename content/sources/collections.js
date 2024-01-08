9e9fcdc7af5e2d4b1ee69c07a79f8a99136ff516
Ronaldo de Souza Maciel <rsmaciel@recordtv.com.br>
2024-01-08 14:43:08 +0000

    Feat/dr216 grids layouts

--- content/sources/collections.js
+++ content/sources/collections.js
@@ -2,7 +2,6 @@ const resolve = function resolve(key) {
   const website = key['arc-site']
   const fromQuery = key.from !== '' && key.from !== undefined ? '&from=' + key.from : ''
   const sizeQuery = key.size !== '' && key.size !== undefined ? '&size=' + key.size : ''
-
   return `/content/v4/collections/?_id=${key._id}&website=${website}${fromQuery}${sizeQuery}`
 }
 
