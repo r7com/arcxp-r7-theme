3365aee956c6c952bf6b5214d0209e41544fdced
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-08 15:43:53 -0300

    feat(url-description): adds new custom field for url description (title atribute)

--- components/features/ui-section-footer/default.jsx
+++ components/features/ui-section-footer/default.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { SectionFoot } from '@r7/ui-section-foot'
 import '@r7/ui-section-foot/style.css'
+import { SectionFoot } from '@r7/ui-section-foot'
 
 const SectionFootBlock = props => {
-  const { background, text, url } = props.customFields
+  const { background, title, urlDescription, url } = props.customFields
+
   return (
     <SectionFoot bg={background || undefined}>
       <SectionFoot.Line />
 
-      <SectionFoot.Link href={url} title={text}>
-        {text}
+      <SectionFoot.Link href={url} title={urlDescription ?? title}>
+        {title}
       </SectionFoot.Link>
     </SectionFoot>
   )
 }
 
 SectionFootBlock.label = 'Section Foot - R7 Block'
+
 SectionFootBlock.icon = 'arc-headline'
 
 SectionFootBlock.propTypes = {
   customFields: PropTypes.shape({
-    text: PropTypes.string.tag({
-      label: 'Texto',
-      description: 'Texto que sera mostrado no fim da sessão"',
+    title: PropTypes.string.tag({
+      label: 'Título',
+      description: 'Texto que sera mostrado no fim da sessão',
     }),
     url: PropTypes.string.tag({
       label: 'URL da seção (https)',
       description: 'Link para saber mais sobre a sessão',
+    }),
+    urlDescription: PropTypes.string.tag({
+      label: 'Descrição da URL',
+      description: 'Descreva a seção/conteúdo do link destino (não deve ser igual ao título)',
     }),
     background: PropTypes.string.tag({
       label: 'Cor da seção (hexadecimal/rgb)',
@@ -35,4 +41,5 @@ SectionFootBlock.propTypes = {
     }),
   }),
 }
+
 export default SectionFootBlock
