7d2cec3a57171b3894630d2b06fb534973fc905e
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-23 19:46:22 -0300

    feat(three-cards-landscape-small): new compost three-card-landscape-small card/widget custom block

---
+++ components/features/three-cards-landscape-small/default.jsx
@@ -0,0 +1,72 @@
+import '@r7/ui-card/style.css'
+import React from 'react'
+import PropTypes from '@arc-fusion/prop-types'
+import { RESIZER_TOKEN_VERSION } from 'fusion:environment'
+import { Image } from '@wpmedia/arc-themes-components'
+import getResizeParamsFromANSImage from '../../../util/get-resize-params-from-ans-image'
+import { LeftPhoto } from '@r7/ui-card'
+import { withCard, getCardPropTypes, CardLabel, CardHat } from '../../../util/card'
+
+const ThreeCardsLandscapeSmall = withCard(
+  props => {
+    const { collection, siteProperties, fusionContext } = props.cardProps
+    const { arcSite } = fusionContext
+    const { fallbackImage, fallbackImageAlt } = siteProperties
+
+    return (
+      <LeftPhoto>
+        {collection.map(item => (
+          <LeftPhoto.Item key={item._id}>
+            <LeftPhoto.Flex>
+              <LeftPhoto.Figure format="landscape">
+                <a href={item.canonical_url} title={item.headlines?.basic}>
+                  {item.promo_items?.basic ? (
+                    <Image
+                      {...getResizeParamsFromANSImage(item.promo_items?.basic, arcSite, 113, [113])}
+                      alt={item.promo_items?.basic.alt_text}
+                      sizes={[{ isDefault: true, sourceSizeValue: '113px' }]}
+                      height={65}
+                      resizedOptions={{
+                        auth: item.promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
+                        smart: true,
+                      }}
+                    />
+                  ) : (
+                    <img
+                      src={fallbackImage}
+                      alt={fallbackImageAlt}
+                      width={113}
+                      height={65}
+                      style={{ height: '100%', objectFit: 'contain' }}
+                    />
+                  )}
+
+                  <CardLabel {...item} />
+                </a>
+              </LeftPhoto.Figure>
+
+              <LeftPhoto.TextWrapper>
+                <CardHat {...item} />
+
+                <LeftPhoto.Title fontSize="small">
+                  <a href={item.canonical_url} title={item.headlines?.basic}>
+                    {item.headlines?.basic}
+                  </a>
+                </LeftPhoto.Title>
+              </LeftPhoto.TextWrapper>
+            </LeftPhoto.Flex>
+          </LeftPhoto.Item>
+        ))}
+      </LeftPhoto>
+    )
+  },
+  { defaultFrom: '0', defaultSize: '3', length: 3 },
+)
+
+ThreeCardsLandscapeSmall.label = 'Três chamadas retangular - R7 Block'
+
+ThreeCardsLandscapeSmall.propTypes = {
+  customFields: PropTypes.shape({ ...getCardPropTypes(2) }),
+}
+
+export default ThreeCardsLandscapeSmall
