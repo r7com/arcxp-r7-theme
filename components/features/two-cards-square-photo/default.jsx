b3c2e0c9459ce864ff539820a7253aa66f9fd194
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-23 19:47:44 -0300

    refactor(two-cards-square-photo): using the new composite sub-components structure

--- components/features/two-cards-square-photo/default.jsx
+++ components/features/two-cards-square-photo/default.jsx
@@ -4,8 +4,8 @@ import PropTypes from '@arc-fusion/prop-types'
 import { RESIZER_TOKEN_VERSION } from 'fusion:environment'
 import { Image } from '@wpmedia/arc-themes-components'
 import getResizeParamsFromANSImage from '../../../util/get-resize-params-from-ans-image'
-import { TwoCardsSquarePhoto } from '@r7/ui-card'
-import { withCard, getCardPropTypes, CardLabel } from '../../../util/card'
+import { LeftPhoto } from '@r7/ui-card'
+import { withCard, getCardPropTypes, CardLabel, CardHat } from '../../../util/card'
 
 const TwoCardsSquarePhotoBlock = withCard(
   props => {
@@ -14,48 +14,57 @@ const TwoCardsSquarePhotoBlock = withCard(
     const { fallbackImage, fallbackImageAlt } = siteProperties
 
     return (
-      <TwoCardsSquarePhoto>
+      <LeftPhoto>
         {collection.map(item => (
-          <TwoCardsSquarePhoto.Item
-            key={item._id}
-            hat={item.taxonomy?.primary_section?.name}
-            title={item.headlines?.basic}
-            renderImage={
-              <a href={collection[0].canonical_url} title={collection[0].headlines?.basic}>
-                {item.promo_items?.basic ? (
-                  <Image
-                    {...getResizeParamsFromANSImage(item.promo_items?.basic, arcSite, 113, [113])}
-                    alt={item.promo_items?.basic.alt_text}
-                    sizes={[{ isDefault: true, sourceSizeValue: '113px' }]}
-                    height={113}
-                    resizedOptions={{
-                      auth: item.promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
-                      smart: true,
-                    }}
-                  />
-                ) : (
-                  <img
-                    src={fallbackImage}
-                    alt={fallbackImageAlt}
-                    width={113}
-                    height={113}
-                    style={{ objectFit: 'cover' }}
-                  />
-                )}
+          <LeftPhoto.Item key={item._id}>
+            <LeftPhoto.Flex>
+              <LeftPhoto.Figure format="square">
+                <a href={item.canonical_url} title={item.headlines?.basic}>
+                  {item.promo_items?.basic ? (
+                    <Image
+                      {...getResizeParamsFromANSImage(item.promo_items?.basic, arcSite, 113, [113])}
+                      alt={item.promo_items?.basic.alt_text}
+                      sizes={[{ isDefault: true, sourceSizeValue: '113px' }]}
+                      height={113}
+                      resizedOptions={{
+                        auth: item.promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
+                        smart: true,
+                      }}
+                    />
+                  ) : (
+                    <img
+                      src={fallbackImage}
+                      alt={fallbackImageAlt}
+                      width={113}
+                      height={113}
+                      style={{ objectFit: 'cover' }}
+                    />
+                  )}
 
-                <CardLabel {...item} {...item.customFields.label} />
-              </a>
-            }
-          />
+                  <CardLabel {...item} />
+                </a>
+              </LeftPhoto.Figure>
+
+              <LeftPhoto.TextWrapper>
+                <CardHat {...item} />
+
+                <LeftPhoto.Title fontSize="small">
+                  <a href={item.canonical_url} title={item.headlines?.basic}>
+                    {item.headlines?.basic}
+                  </a>
+                </LeftPhoto.Title>
+              </LeftPhoto.TextWrapper>
+            </LeftPhoto.Flex>
+          </LeftPhoto.Item>
         ))}
-      </TwoCardsSquarePhoto>
+      </LeftPhoto>
     )
   },
   // This might be unnecessary, but without the `length` a `slice` would be necessary
   { defaultFrom: '0', defaultSize: '2', length: 2 },
 )
 
-TwoCardsSquarePhotoBlock.label = 'Two Cards Square Photo - R7 Block'
+TwoCardsSquarePhotoBlock.label = 'Duas chamadas quadradas - R7 Block'
 
 TwoCardsSquarePhotoBlock.propTypes = {
   customFields: PropTypes.shape({ ...getCardPropTypes(2) }),
