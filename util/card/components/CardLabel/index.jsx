1154c8593a911c1d11568911878265908d46a003
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-23 19:38:09 -0300

    feat(card-label): new infered typings and simplified {...item} spread on CardLabel
    This way we can just use <CardLabel {...item}/> to automatic get all the business logic

--- util/card/components/CardLabel/index.jsx
+++ util/card/components/CardLabel/index.jsx
@@ -2,37 +2,38 @@ import React from 'react'
 import { Card } from '@r7/ui-card'
 import { getLabelTypeBySite } from '../../get-label-type-by-site'
 
-/** CardLabel component */
-export const CardLabel = ({
-  displayLabel,
-  label,
-  sponsoredBy,
-  sponsoredByImage,
-  sponsoredByImageDesc,
-  sponsoredByTitle,
-  sponsoredByUrl,
-  taxonomy,
-  fallbackImage,
-}) => {
+/**
+ * CardLabel component
+ * @typedef {{fallbackImage:string}} CardLabelProps
+ * @typedef {ReturnType<typeof import("../../useCard").useCard>} UseCardReturn
+ * @param {UseCardReturn["collection"][number]} - CardCollection
+ */
+export const CardLabel = ({ customFields, taxonomy, fallbackImage }) => {
   const labelBySite = getLabelTypeBySite({ taxonomy })
 
-  if (!displayLabel && !sponsoredBy) return null
+  if (!customFields.displayLabel && !customFields.sponsoredBy) return null
 
   // Automatic label but didn't find any label by the path
-  if (label === 'automatic') {
+  if (customFields.label === 'automatic') {
     if (!labelBySite) return null
   } else {
     // Is manual label, but didn't select any option
-    if (!label) return null
+    if (!customFields.label) return null
   }
 
   return (
     <Card.Label
-      sponsoredByImage={sponsoredByImage ?? fallbackImage}
-      sponsoredByImageDesc={sponsoredByImageDesc}
-      sponsoredByTitle={sponsoredByTitle}
-      sponsoredByUrl={sponsoredByUrl}
-      type={sponsoredBy ? 'sponsored-by' : label === 'automatic' ? labelBySite : label}
+      sponsoredByImage={customFields.sponsoredByImage ?? fallbackImage}
+      sponsoredByImageDesc={customFields.sponsoredByImageDesc}
+      sponsoredByTitle={customFields.sponsoredByTitle}
+      sponsoredByUrl={customFields.sponsoredByUrl}
+      type={
+        customFields.sponsoredBy
+          ? 'sponsored-by'
+          : customFields.label === 'automatic'
+          ? labelBySite
+          : customFields.label
+      }
     />
   )
 }
