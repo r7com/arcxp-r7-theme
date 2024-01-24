afcac6ab9923bb469487f08305a1c450292d63cf
Jorge Santos <jldsantos@rederecord.com.br>
2024-01-24 12:01:16 -0300

    fix(card-label): fixes CardLabel props object using the wrong ones
    The label related props comes from customFields.label, not from customFields.

--- util/card/components/CardLabel/index.jsx
+++ util/card/components/CardLabel/index.jsx
@@ -11,29 +11,33 @@ import { getLabelTypeBySite } from '../../get-label-type-by-site'
 export const CardLabel = ({ customFields, taxonomy, fallbackImage }) => {
   const labelBySite = getLabelTypeBySite({ taxonomy })
 
-  if (!customFields.displayLabel && !customFields.sponsoredBy) return null
+  const {
+    displayLabel,
+    sponsoredBy,
+    label,
+    sponsoredByImage,
+    sponsoredByImageDesc,
+    sponsoredByTitle,
+    sponsoredByUrl,
+  } = customFields.label
+
+  if (!displayLabel && !sponsoredBy) return null
 
   // Automatic label but didn't find any label by the path
-  if (customFields.label === 'automatic') {
+  if (label === 'automatic') {
     if (!labelBySite) return null
   } else {
     // Is manual label, but didn't select any option
-    if (!customFields.label) return null
+    if (!label) return null
   }
 
   return (
     <Card.Label
-      sponsoredByImage={customFields.sponsoredByImage ?? fallbackImage}
-      sponsoredByImageDesc={customFields.sponsoredByImageDesc}
-      sponsoredByTitle={customFields.sponsoredByTitle}
-      sponsoredByUrl={customFields.sponsoredByUrl}
-      type={
-        customFields.sponsoredBy
-          ? 'sponsored-by'
-          : customFields.label === 'automatic'
-          ? labelBySite
-          : customFields.label
-      }
+      sponsoredByImage={sponsoredByImage ?? fallbackImage}
+      sponsoredByImageDesc={sponsoredByImageDesc}
+      sponsoredByTitle={sponsoredByTitle}
+      sponsoredByUrl={sponsoredByUrl}
+      type={sponsoredBy ? 'sponsored-by' : label === 'automatic' ? labelBySite : label}
     />
   )
 }
