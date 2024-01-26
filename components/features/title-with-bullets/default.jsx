6bb388e5f98891bdbe09f7ee45a02e32480bc9ee
Alescia Fernandes <alfernandes@recordtv.com.br>
2024-01-26 10:02:33 -0300

    refactor(title-with-bullets): using Guerra to build title-with-bullets

--- components/features/title-with-bullets/default.jsx
+++ components/features/title-with-bullets/default.jsx
@@ -1,27 +1,30 @@
 import '@r7/ui-card/style.css'
 import React from 'react'
 import PropTypes from '@arc-fusion/prop-types'
-import { TitleWithBullets } from '@r7/ui-card'
+import { Guerra } from '@r7/ui-card'
 import { Bullet } from '@r7/ui-base-components'
-import { withCard } from '../../../util/card'
+import { CardHat, withCard } from '../../../util/card'
 
 const TitleWithBulletsBlock = withCard(
   props => {
     const { collection } = props.cardProps
     return (
-      <TitleWithBullets
-        hat={collection[0].taxonomy?.primary_section?.name}
-        title={collection[0].headlines?.basic}
-        renderBullets={
-          <>
-            {collection.slice(1).map(item => (
-              <Bullet key={item._id} url={item.canonical_url}>
-                {item.headlines.basic}
-              </Bullet>
-            ))}
-          </>
-        }
-      />
+      <Guerra>
+        <CardHat {...collection[0]} color="low" />
+
+        <Guerra.Title color="low">
+          <a href={collection[0].canonical_url} title={collection[0].headlines?.basic}>
+            {collection[0].headlines?.basic}
+          </a>
+        </Guerra.Title>
+        <Guerra.Bullets>
+          {collection.slice(1).map(item => (
+            <Bullet key={item._id} url={item.canonical_url}>
+              {item.headlines.basic}
+            </Bullet>
+          ))}
+        </Guerra.Bullets>
+      </Guerra>
     )
   },
   { defaultFrom: '0', defaultSize: '5', length: 5 },
