4dcbc38b5b7aebf17292279dd662664c55efc593
Alescia Fernandes <alfernandes@recordtv.com.br>
2024-01-26 10:51:00 -0300

    feat(title-with-bullets): added global content suport

--- components/features/title-with-bullets/default.jsx
+++ components/features/title-with-bullets/default.jsx
@@ -35,6 +35,21 @@ TitleWithBulletsBlock.label = 'Título com bullets - R7 Block'
 
 TitleWithBulletsBlock.propTypes = {
   customFields: PropTypes.shape({
+    isGlobalContent: PropTypes.boolean.tag({
+      group: 'Configurar conteúdo',
+      label: 'Usar conteúdo global',
+      default: false,
+    }),
+    globalContentFrom: PropTypes.number.tag({
+      group: 'Configurar conteúdo',
+      label: 'from global',
+      defaultValue: 0,
+    }),
+    globalContentSize: PropTypes.number.tag({
+      group: 'Configurar conteúdo',
+      label: 'size global',
+      defaultValue: 0,
+    }),
     config: PropTypes.contentConfig().tag({
       group: 'Configurar conteúdo',
       label: 'Fonte de conteúdo',
