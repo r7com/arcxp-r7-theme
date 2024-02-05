ba1ce0785d74397cdf0a293a2b7e79f710759931
Moiteiro <bmoiteiro@recordtv.com.br>
2024-02-05 16:32:02 +0000

    Adding comments to guide Origin change requests

--- content/sources/content-api-redirect.js
+++ content/sources/content-api-redirect.js
@@ -44,6 +44,37 @@ const fetch = ({ _id, 'arc-site': website, website_url: websiteUrl }, { cachedCa
           path: websiteUrl,
           host: website === 'r7' ? `${website}.com` : `${website}.r7.com`,
         })
+
+        // ==== R E Q U E S T ====
+
+        // 1 - validar se o fim da URL contem uma data antes da migração. É importante ter certeza que o 
+        // artigo foi criado no Prost para evitar requisições desnecessárias
+
+        // 2 - remover a barra final da url antes de montar a requisição para o Prost. O ARC adiciona essa barra 
+        // por padrão:
+        // www.r7.com/minha-materia-05022024/ remover barra e ficar assim: www.r7.com/minha-materia-05022024
+
+        // 3 - fazer a requisição para um endpoint (a ser definido. coloca em uma config) adicionando header Host
+        // para que o varnish saiba tratar
+        
+        // 4 - Adiciona API (ainda a ser definido, hoje nao é necessário) para autenticar na CDN
+
+        // exemplo: curl --location 'https://entretenimento.r7.com/economia/banco-central-define-nova-taxa-basica-de-juros-nesta-quarta-feira-31012024' --header 'Host: noticias.r7.com' --header 'Authorization: Bearer MEU-TOKEN-SECRETO'
+
+        // ==== R E S P O N S E ====
+        // 200 - entregar exatamente o que for recebido. Não podemos adicionar nenhum HTML extra porque pode
+        // esse tipo de resposta é de alguma aplicação legada ou de conteúdo não migrado que já deve ter todo a página
+        // pronta a ser entregue
+
+        // 302 - Pode ser um redirect do prost de uma URL antiga ou resolução de uma URL encurtada. Devemos entregar essa 
+        // resposta para o usuário sem que haja auteração no payload ou cabeçalhos.
+
+        // 404 - Adicionar o 404 do ARC no lugar do 404 do schumi. Vamos trocar a página para exibir o que quer
+        // tenha sido criado na página 404 da migração
+        
+        // 500 - Entregar a página de erro do ARC no lugar do conteúdo vindo do schumi.
+        
+
         return axios
           .get({
             url: `http://prost-delivery.ir7.com.br/api/resource/published_version?${searchParams.toString()}`,
