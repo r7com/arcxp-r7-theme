d9d3fdfd2ab80cdc34fb8b05b7fda7464f290775
abcosta <abcosta@recordtv.com.br>
2024-02-07 09:58:17 -0300

    refactor: removed validation collection

--- components/features/most-read/default.jsx
+++ components/features/most-read/default.jsx
@@ -19,52 +19,51 @@ const MostReadBlock = withCard(
           Mais Lidas
         </Typography>
         <MostRead.List>
-          {collection &&
-            collection.map(({ headlines, _id, canonical_url, promo_items, taxonomy }, i) => {
-              const {
-                primary_section: { name },
-              } = taxonomy
-              const order = i + 1
-              return (
-                <MostRead.Item key={_id}>
-                  <MostRead.Link url={canonical_url} title={headlines.basic}>
-                    <MostRead.Figure>
-                      {promo_items?.basic ? (
-                        <Image
-                          {...getResizeParamsFromANSImage(promo_items?.basic, arcSite, 141, [141])}
-                          alt={promo_items?.basic.alt_text}
-                          sizes={[{ isDefault: true, sourceSizeValue: '141px' }]}
-                          height={141}
-                          resizedOptions={{
-                            auth: promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
-                            smart: true,
-                          }}
-                          style={{
-                            width: 141,
-                            height: 141,
-                            objectFit: 'contain',
-                            marginBottom: '16px',
-                            borderRadius: '4px',
-                          }}
-                        />
-                      ) : (
-                        <img
-                          src={fallbackImage}
-                          alt={fallbackImageAlt}
-                          width={141}
-                          height={141}
-                          style={{ width: 141, height: 141, objectFit: 'contain' }}
-                        />
-                      )}
-                    </MostRead.Figure>
-                    <MostRead.Order order={order}>
-                      <MostRead.Hat>{name}</MostRead.Hat>
-                      <MostRead.Title>{headlines.basic}</MostRead.Title>
-                    </MostRead.Order>
-                  </MostRead.Link>
-                </MostRead.Item>
-              )
-            })}
+          {collection.map(({ headlines, _id, canonical_url, promo_items, taxonomy }, i) => {
+            const {
+              primary_section: { name },
+            } = taxonomy
+            const order = i + 1
+            return (
+              <MostRead.Item key={_id}>
+                <MostRead.Link url={canonical_url} title={headlines.basic}>
+                  <MostRead.Figure>
+                    {promo_items?.basic ? (
+                      <Image
+                        {...getResizeParamsFromANSImage(promo_items?.basic, arcSite, 141, [141])}
+                        alt={promo_items?.basic.alt_text}
+                        sizes={[{ isDefault: true, sourceSizeValue: '141px' }]}
+                        height={141}
+                        resizedOptions={{
+                          auth: promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
+                          smart: true,
+                        }}
+                        style={{
+                          width: 141,
+                          height: 141,
+                          objectFit: 'contain',
+                          marginBottom: '16px',
+                          borderRadius: '4px',
+                        }}
+                      />
+                    ) : (
+                      <img
+                        src={fallbackImage}
+                        alt={fallbackImageAlt}
+                        width={141}
+                        height={141}
+                        style={{ width: 141, height: 141, objectFit: 'contain' }}
+                      />
+                    )}
+                  </MostRead.Figure>
+                  <MostRead.Order order={order}>
+                    <MostRead.Hat>{name}</MostRead.Hat>
+                    <MostRead.Title>{headlines.basic}</MostRead.Title>
+                  </MostRead.Order>
+                </MostRead.Link>
+              </MostRead.Item>
+            )
+          })}
         </MostRead.List>
       </MostRead>
     )
