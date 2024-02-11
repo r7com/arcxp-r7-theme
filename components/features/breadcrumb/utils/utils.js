84ac59ab358e61d2d6ad1543bc059fc7f2dbcf5f
stefan9541 <45336682+stefan9541@users.noreply.github.com>
2024-02-11 14:30:14 +0200

    fix: bread crumbs for sectino & grouping templates

--- components/features/breadcrumb/utils/utils.js
+++ components/features/breadcrumb/utils/utils.js
@@ -1,27 +1,73 @@
-export const breadcrumbProxy = (globalContent, websiteDomain) => {
-  if (!globalContent || !websiteDomain) return null
+import { useFusionContext } from 'fusion:context'
+import getProperties from 'fusion:properties'
+import { useSiteSectionProp } from '../../../../util/useSiteTopperProp'
+const SECTION_NAME = 'navigation.nav_title'
 
-  const { websites, website } = globalContent
-  const path = websites && websites[website]?.website_section?.path
-  const sectionNames = path?.replace('/', '')?.replaceAll('-', ' ')?.split('/')
-  sectionNames?.unshift(website)
-
-  return (
-    sectionNames?.length &&
-    sectionNames?.map((sectionName, index) => {
-      const url = createURL({ websiteDomain, sectionNames, index })
-      return {
-        name: sectionName,
-        url,
-        id: `${sectionName}${index}`,
-      }
-    })
-  )
+function countSlashes(obj) {
+  return (obj.url.match(/\//g) || []).length
 }
 
-const createURL = ({ websiteDomain, sectionNames, index }) => {
-  return `${websiteDomain}/${sectionNames
-    ?.slice(1, index + 1)
-    ?.join('/')
-    ?.replaceAll(' ', '-')}`
+export const breadcrumbProxy = () => {
+  const { globalContent, arcSite, metaValue } = useFusionContext()
+  const { websiteDomain, websiteName } = getProperties(arcSite)
+
+  if (!globalContent || !websiteDomain) return null
+
+  const pageType = metaValue('page-type')
+  const isArticle = pageType === 'article' || pageType === 'gallery'
+  const isSection = pageType === 'section' || pageType === 'grouping'
+
+  const defaultSection = [
+    {
+      name: websiteName,
+      url: websiteDomain,
+      id: websiteDomain,
+    },
+  ]
+
+  if (isArticle) {
+    const { taxonomy, canonical_website } = globalContent
+    const sections = taxonomy?.sections || []
+
+    const sectionNames = sections
+      .filter(el => el._website === canonical_website)
+      .map(el => {
+        return {
+          url: el.path,
+          name: el.name,
+        }
+      })
+      .sort((a, b) => countSlashes(a) - countSlashes(b))
+    console.log('sectionNames', sectionNames)
+
+    sectionNames.forEach(el => {
+      defaultSection.push({
+        name: el.name,
+        url: `${websiteDomain}${el.url}`,
+        id: `${websiteDomain}${el.url}`,
+      })
+    })
+    return defaultSection
+  }
+  if (isSection) {
+    const { _id } = globalContent
+    const splitSctions = _id.split('/').filter(el => !!el)
+    const sectionIds = splitSctions.map(
+      (element, index) => '/' + splitSctions.slice(0, index + 1).join('/'),
+    )
+    const sectionNames = sectionIds.map(el => {
+      return {
+        url: el,
+        name: useSiteSectionProp(SECTION_NAME, el),
+      }
+    })
+    sectionNames.forEach(el => {
+      defaultSection.push({
+        name: el.name,
+        url: `${websiteDomain}${el.url}`,
+        id: `${websiteDomain}${el.url}`,
+      })
+    })
+    return defaultSection
+  }
 }
