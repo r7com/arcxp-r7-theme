abcb8606b8450736c2e576c6e79a74013712ce36
Kelly Guimarans <kguimarans@recordtv.com.br>
2024-02-22 09:32:37 -0300

    feat: two-call component completion

---
+++ components/features/proportional-photo-call-text/default.jsx
@@ -0,0 +1,74 @@
+import '@r7/ui-card/style.css'
+import React from 'react'
+import PropTypes from '@arc-fusion/prop-types'
+import { RESIZER_TOKEN_VERSION } from 'fusion:environment'
+import { Image } from '@wpmedia/arc-themes-components'
+import getResizeParamsFromANSImage from '../../../util/get-resize-params-from-ans-image'
+import { ProportionalPhoto } from '@r7/ui-card'
+import { withCard, getCardPropTypes, CardHat, CardLabel } from '../../../util/card'
+
+const PrimaryCallPhotoTextSecondText = withCard(
+  props => {
+    const { collection, siteProperties, fusionContext } = props.cardProps
+    const { arcSite } = fusionContext
+    const { fallbackImage, fallbackImageAlt } = siteProperties
+
+    return (
+      <ProportionalPhoto>
+        <ProportionalPhoto.Figure>
+          <a href={collection[0].canonical_url}>
+            {collection[0].promo_items?.basic ? (
+              <Image
+                {...getResizeParamsFromANSImage(collection[0].promo_items?.basic, arcSite, 348, [
+                  348,
+                ])}
+                alt={collection[0].promo_items?.basic.alt_text}
+                sizes={[{ isDefault: true, sourceSizeValue: '348px' }]}
+                height={199}
+                resizedOptions={{
+                  auth: collection[0].promo_items?.basic.auth[RESIZER_TOKEN_VERSION],
+                  smart: true,
+                }}
+                style={{ width: '100%', height: 'auto' }}
+              />
+            ) : (
+              <img
+                src={fallbackImage}
+                alt={fallbackImageAlt}
+                style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+              />
+            )}
+            <CardLabel {...collection[0]} />
+          </a>
+        </ProportionalPhoto.Figure>
+
+        <ProportionalPhoto.TextWrapper marginBottom>
+          <CardHat {...collection[0]} />
+
+          <a href={collection[0].canonical_url} title={collection[0].headlines?.basic}>
+            <ProportionalPhoto.Title>{collection[0].headlines?.basic}</ProportionalPhoto.Title>
+          </a>
+        </ProportionalPhoto.TextWrapper>
+
+        <ProportionalPhoto.TextWrapper>
+          <CardHat {...collection[1]} />
+          <a href={collection[1].canonical_url} title={collection[1].headlines?.basic}>
+            <ProportionalPhoto.Title>{collection[1].headlines?.basic}</ProportionalPhoto.Title>
+          </a>
+        </ProportionalPhoto.TextWrapper>
+      </ProportionalPhoto>
+    )
+  },
+  { defaultFrom: '0', defaultSize: '2', length: 2 },
+)
+
+PrimaryCallPhotoTextSecondText.label =
+  'Chamada com foto e texto segunda chamada com texto - R7 Block'
+
+PrimaryCallPhotoTextSecondText.propTypes = {
+  customFields: PropTypes.shape({
+    ...getCardPropTypes(1),
+  }),
+}
+
+export default PrimaryCallPhotoTextSecondText
