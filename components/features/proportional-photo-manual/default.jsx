a3d84b3b88546531300af3f286d55ac605dd0087
Kelly Guimarans <kguimarans@recordtv.com.br>
2024-02-29 15:04:28 -0300

    feat: implementation of the proportion photo manul component

---
+++ components/features/proportional-photo-manual/default.jsx
@@ -0,0 +1,66 @@
+import '@r7/ui-card/style.css'
+import React from 'react'
+import PropTypes from '@arc-fusion/prop-types'
+import { Card, ProportionalPhoto } from '@r7/ui-card'
+
+const ProportionalPhotoBlock = props => {
+  const { url, imageURL, title, hatURL, hatTitle } = props.customFields
+
+  return (
+    <ProportionalPhoto>
+      <ProportionalPhoto.Figure>
+        <a href={url}>
+          <img
+            data-tb-thumbnail
+            src={imageURL}
+            alt={title}
+            style={{ objectFit: 'contain', width: '100%', height: '100%' }}
+          />
+        </a>
+      </ProportionalPhoto.Figure>
+
+      <ProportionalPhoto.TextWrapper>
+        <Card.HatWrapper>
+          <a href={hatURL} title={hatTitle}>
+            <Card.HatTitle>{hatTitle}</Card.HatTitle>
+          </a>
+        </Card.HatWrapper>
+
+        <ProportionalPhoto.Title>
+          <a href={url} title={title}>
+            {title}
+          </a>
+        </ProportionalPhoto.Title>
+      </ProportionalPhoto.TextWrapper>
+    </ProportionalPhoto>
+  )
+}
+
+ProportionalPhotoBlock.label = 'Foto Média Manual - R7 Block'
+
+ProportionalPhotoBlock.propTypes = {
+  customFields: PropTypes.shape({
+    url: PropTypes.string.tag({
+      group: `Chamada ${counter}`,
+      label: 'Link de destino da chamada',
+    }),
+    imageURL: PropTypes.string.tag({
+      group: `Chamada ${counter}`,
+      label: 'URL da imagem da chamada (https)',
+    }),
+    title: PropTypes.string.tag({
+      group: `Chamada ${counter}`,
+      label: 'Título da chamada',
+    }),
+    hatURL: PropTypes.string.tag({
+      group: `Chamada ${counter}`,
+      label: 'Título do chapéu',
+    }),
+    hatTitle: PropTypes.string.tag({
+      group: `Chamada ${counter}`,
+      label: 'Link de destino do chapéu',
+    }),
+  }),
+}
+
+export default ProportionalPhotoBlock
