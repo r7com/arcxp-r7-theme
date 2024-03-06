27d85659135c69924b70409d0471f90437cbe40b
Kelly Guimarans <kguimarans@recordtv.com.br>
2024-03-06 08:53:02 -0300

    fix: adjustment in the way the image is implemented and removed from the group

--- components/features/proportional-photo-manual/default.jsx
+++ components/features/proportional-photo-manual/default.jsx
@@ -4,7 +4,7 @@ import PropTypes from '@arc-fusion/prop-types'
 import { Card, ProportionalPhoto } from '@r7/ui-card'
 
 const ProportionalPhotoBlock = props => {
-  const { url, imageURL, title, hatURL, hatTitle } = props.customFields
+  const { url, imageURL, alt, title, hatURL, hatTitle } = props.customFields
 
   return (
     <ProportionalPhoto>
@@ -13,7 +13,7 @@ const ProportionalPhotoBlock = props => {
           <img
             data-tb-thumbnail
             src={imageURL}
-            alt={title}
+            alt={alt}
             style={{ objectFit: 'contain', width: '100%', height: '100%' }}
           />
         </a>
@@ -41,24 +41,22 @@ ProportionalPhotoBlock.label = 'Foto Média Manual - R7 Block'
 ProportionalPhotoBlock.propTypes = {
   customFields: PropTypes.shape({
     url: PropTypes.string.tag({
-      group: `Chamada`,
       label: 'Link de destino da chamada',
     }),
     imageURL: PropTypes.string.tag({
-      group: `Chamada`,
       label: 'Escolha a imagem',
       searchable: 'image',
     }),
+    alt: PropTypes.string.tag({
+      label: 'Texto alternativo da imagem',
+    }),
     title: PropTypes.string.tag({
-      group: `Chamada`,
       label: 'Título da chamada',
     }),
     hatTitle: PropTypes.string.tag({
-      group: `Chamada`,
       label: 'Título do chapéu',
     }),
     hatURL: PropTypes.string.tag({
-      group: `Chamada`,
       label: 'Link de destino do chapéu',
     }),
   }),
