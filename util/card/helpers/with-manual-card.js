3322ad1374d43bc08c7bb0e3e176b6ee031d46e9
rafaelfs96 <rafaelfs96@outlook.com>
2024-03-12 15:38:38 -0300

    chore(utils/cards): fixing with-manual-cards comments

--- util/card/helpers/with-manual-card.js
+++ util/card/helpers/with-manual-card.js
@@ -5,7 +5,7 @@ import { useManualCard } from './use-manual-card'
 import { RenderCard } from '../components/RenderCard'
 
 /**
- * Function withCard adds a card wrapper around a provided component.
+ * Function withManualCards adds a card wrapper around a provided component.
  * @param {React.ComponentType<{cardProps:ReturnType<useManualCard>}>} Component The component
  * @param {import("./use-manual-card").CardOptions} options Options for the card
  * @returns {(props: {customFields: Object}) => React.Component} The wrapped component with a card
