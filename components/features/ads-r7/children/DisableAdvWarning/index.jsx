a51ef1636e63be8816f205355577c288f6d4a622
Alescia Fernandes <alfernandes@recordtv.com.br>
2024-03-14 14:42:59 -0300

    feat(ads): using disclaimer to show disable warning

--- components/features/ads-r7/children/DisableAdvWarning/index.jsx
+++ components/features/ads-r7/children/DisableAdvWarning/index.jsx
@@ -1,11 +1,12 @@
+import { Disclaimer } from '@r7/ui-base-components'
 import React from 'react'
 
 function DisabledAdvWarning() {
   return (
-    <p style={{ textAlign: 'center', padding: '5px', backgroundColor: '#ffefd5' }}>
+    <Disclaimer bgColor="#ffefd5" color="low">
       Esta página está configurada para não exibir publicidades.
-      <strong>Esta peça não será exibida.</strong>
-    </p>
+      <strong> Esta peça não será exibida.</strong>
+    </Disclaimer>
   )
 }
 
